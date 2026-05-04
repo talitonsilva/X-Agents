3840f2f73e22a4a59373315cfbfa1bb9fb8303f6
Taliton Silva <talitonsilva@users.noreply.github.com>
2026-05-04 17:16:05 -0300

    Republish public release artifacts 2026.05.04-r84

--- releases/2026.05.04-r84/install.sh
+++ releases/2026.05.04-r84/install.sh
@@ -6,7 +6,7 @@ TMP_DIR="$(mktemp -d /tmp/xagents-public-install.XXXXXX)"
 VERSION="2026.05.04-r84"
 RELEASE_BASE_URL="https://github.com/talitonsilva/X-Agents/releases/download/2026.05.04-r84"
 ARCHIVE_URL="https://github.com/talitonsilva/X-Agents/releases/download/2026.05.04-r84/xagents-2026.05.04-r84.tar.gz"
-ARCHIVE_SHA256="78eed2e89f44cfa370251508a508d4deb8055ad7cdf68706777eb4700bc59483"
+ARCHIVE_SHA256="7dd4e781be3c78d12e9e5946e53dbd3d0dc902ff4a0294f5067c5e0adb18e07f"
 
 if [[ -t 1 ]]; then
   C_RESET="$(printf '\033[0m')"
